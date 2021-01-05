e09cc0839cc19b12e0a13e869aa4855315e08160
Frank Yoo <frank.yoo@fortanix.com>
2021-01-05 13:47:10 +1100

    [A] adding group as data source

---
+++ sdkms/data_source_group.go
@@ -0,0 +1,88 @@
+// **********
+// Terraform Provider - SDKMS: data source: group
+// **********
+//       - Author:    fyoo at fortanix dot com
+//       - Version:   0.1
+//       - Date:      05/01/2021
+// **********
+
+package sdkms
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceGroup() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceGroupRead,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"group_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"acct_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"creator": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceGroupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	req, err := m.(*api_client).APICallList("GET", "sys/v1/groups")
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Unable to call SDKMS provider API client",
+			Detail:   fmt.Sprintf("[E]: API: GET sys/v1/groups: %s", err),
+		})
+		return diags
+	} 
+		//	Detail:   fmt.Sprintf("%s", req[0].(map[string]interface{})["group_id"]),
+	
+	for _, data := range req {
+		if data.(map[string]interface{})["name"].(string) == d.Get("name").(string) {
+			if err := d.Set("name", data.(map[string]interface{})["name"].(string)); err != nil {
+				return diag.FromErr(err)
+			}
+			if err := d.Set("group_id", data.(map[string]interface{})["group_id"].(string)); err != nil {
+				return diag.FromErr(err)
+			}
+			if err := d.Set("acct_id", data.(map[string]interface{})["acct_id"].(string)); err != nil {
+				return diag.FromErr(err)
+			}
+			if err := d.Set("creator", data.(map[string]interface{})["creator"]); err != nil {
+				return diag.FromErr(err)
+			}
+			if _, ok := data.(map[string]interface{})["description"]; ok {
+				if err := d.Set("description", data.(map[string]interface{})["description"].(string)); err != nil {
+					return diag.FromErr(err)
+				}
+			}
+		}
+	}
+
+	d.SetId(d.Get("group_id").(string))
+	return nil
+}
