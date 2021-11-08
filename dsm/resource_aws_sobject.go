05880df334f26969921e382ffaf03c590c521749
Frank Yoo <fyoo@outlook.com>
2021-11-08 20:07:03 +1100

    [M] fixing links section

--- dsm/resource_aws_sobject.go
+++ dsm/resource_aws_sobject.go
@@ -295,7 +295,10 @@ func resourceUpdateAWSSobject(ctx context.Context, d *schema.ResourceData, m int
 		update_aws_metadata := map[string]interface{}{
 			"kid": d.Id(),
 		}
-		update_aws_metadata["custom_metadata"] = d.Get("custom_metadata")
+		update_aws_metadata["custom_metadata"] = map[string]interface{}{
+			"aws-aliases": d.Get("custom_metadata").(map[string]interface{})["aws-aliases"],
+			"aws-policy":  d.Get("custom_metadata").(map[string]interface{})["aws-policy"],
+		}
 		_, err := m.(*api_client).APICallBody("PATCH", fmt.Sprintf("crypto/v1/keys/%s", d.Id()), update_aws_metadata)
 		if err != nil {
 			diags = append(diags, diag.Diagnostic{
