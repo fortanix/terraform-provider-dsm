d73d1fe96b0bb6af4b6d1cf1eb595d1de9ad7df3
sanjeev <sanjeev.desai@fortanix.com>
2022-08-04 20:12:44 +0530

    added data source for sobject

---
+++ dsm/data_source_sobject.go
@@ -0,0 +1,113 @@
+// **********
+// Terraform Provider - DSM: data source: secret
+// **********
+//       - Author:    fyoo at fortanix dot com
+//       - Version:   0.3.7
+//       - Date:      28/07/2021
+// **********
+
+package dsm
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceSobject() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceSobjectRead,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"kid": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"acct_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"creator": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"export": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"value": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func dataSourceSobjectRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	security_object := map[string]interface{}{
+		"name": d.Get("name").(string),
+	}
+
+	req, err := m.(*api_client).APICallBody("POST", "crypto/v1/keys/export", security_object)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "[DSM SDK] Unable to call DSM provider API client",
+			Detail:   fmt.Sprintf("[E]: API: POST crypto/v1/keys/export: %v", err),
+		})
+		return diags
+	}
+
+	//req, err := m.(*api_client).APICallList("GET", "crypto/v1/keys")
+	//if err != nil {
+	//	diags = append(diags, diag.Diagnostic{
+	//		Severity: diag.Error,
+	//		Summary:  "[DSM SDK] Unable to call DSM provider API client",
+	//		Detail:   fmt.Sprintf("[E]: API: GET crypto/v1/keys: %s", err),
+	//	})
+	//	return diags
+	//}
+	//	Detail:   fmt.Sprintf("%s", req[0].(map[string]interface{})["group_id"]),
+
+	if err := d.Set("name", req["name"].(string)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("kid", req["kid"].(string)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("acct_id", req["acct_id"].(string)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("creator", req["creator"]); err != nil {
+		return diag.FromErr(err)
+	}
+	if _, ok := req["description"]; ok {
+		if err := d.Set("description", req["description"].(string)); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if d.Get("export").(bool) {
+		if err := d.Set("value", req["value"].(string)); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	d.SetId(d.Get("kid").(string))
+	return nil
+}
