4bb08742656b1e810287fa6e3d950e44a336b7d7
RaviGopal <ravi.gopal@fortanix.com>
2022-09-23 17:26:36 +0530

    Indentation changes.

--- dsm/resource_app.go
+++ dsm/resource_app.go
@@ -73,21 +73,21 @@ func resourceApp() *schema.Resource {
 				Optional: true,
 			},
 			"other_group_permissions": {
-                Type: schema.TypeMap,
-                Optional: true,
-                Elem: &schema.Schema{
-                    Type: schema.TypeString,
-                    Optional: true,
-                },
-            },
-            "mod_group_permissions": {
-                Type: schema.TypeMap,
-                Optional: true,
-                Elem: &schema.Schema{
-                    Type: schema.TypeString,
-                    Optional: true,
-                },
-            },
+				Type: schema.TypeMap,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+					Optional: true,
+				},
+			},
+			"mod_group_permissions": {
+				Type: schema.TypeMap,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+					Optional: true,
+				},
+			},
 		},
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -109,18 +109,16 @@ func resourceCreateApp(ctx context.Context, d *schema.ResourceData, m interface{
 		"description": d.Get("description").(string),
 	}
 
-
     add_group_perms := form_group_permissions(d.Get("other_group_permissions"))
-
-    app_add_group := make(map[string]interface{})
-    if err := d.Get("other_group").([]interface{}); len(err) > 0 {
-	    for _, group_id := range d.Get("other_group").([]interface{}) {
-            if perms, ok := add_group_perms[group_id.(string)]; ok {
-                app_add_group[group_id.(string)] = perms
-            }else{
-                app_add_group[group_id.(string)] = []string{"SIGN", "VERIFY", "ENCRYPT", "DECRYPT", "WRAPKEY", "UNWRAPKEY", "DERIVEKEY", "MACGENERATE", "MACVERIFY", "EXPORT", "MANAGE", "AGREEKEY", "AUDIT"}
-            }
-        }
+	app_add_group := make(map[string]interface{})
+	if err := d.Get("other_group").([]interface{}); len(err) > 0 {
+		for _, group_id := range d.Get("other_group").([]interface{}) {
+			if perms, ok := add_group_perms[group_id.(string)]; ok {
+				app_add_group[group_id.(string)] = perms
+			}else{
+				app_add_group[group_id.(string)] = []string{"SIGN", "VERIFY", "ENCRYPT", "DECRYPT", "WRAPKEY", "UNWRAPKEY", "DERIVEKEY", "MACGENERATE", "MACVERIFY", "EXPORT", "MANAGE", "AGREEKEY", "AUDIT"}
+			}
+		}
 	}
 
     if perms, ok := add_group_perms[d.Get("default_group").(string)]; ok {
@@ -219,82 +217,81 @@ func resourceUpdateApp(ctx context.Context, d *schema.ResourceData, m interface{
 			return diags
 		}
 	}
-
     //Modified by Ravi Gopal
-    app_object := make(map[string]interface{})
+	app_object := make(map[string]interface{})
 
-    if d.HasChange("default_group") {
-        if default_group := d.Get("default_group").(string); len(default_group) > 0 {
-            app_object["default_group"] = d.Get("default_group")
-        }
-    }
-    if d.HasChange("other_group") {
-        old_group, new_group := d.GetChange("other_group")
-        /*compares the old and new state
-         * and seggregtes the new groups and groups that are to be deleted.
-        */
-        add_group_ids, del_group_ids := form_add_and_del_groups(old_group, new_group)
-        //Add the groups to be deleted
-        if len(del_group_ids) > 0 {
-            app_object["del_groups"] = del_group_ids
-        }
-        //Add the new groups
-        if len(add_group_ids) > 0 {
-            add_group_perms := form_group_permissions(d.Get("other_group_permissions"))
-            app_add_group := make(map[string]interface{})
-            for i := 0; i < len(add_group_ids); i++ {
-                if perms, ok := add_group_perms[add_group_ids[i]]; ok {
-                    app_add_group[add_group_ids[i]] = perms
-                }else{
-                    app_add_group[add_group_ids[i]] = []string{"SIGN", "VERIFY", "ENCRYPT", "DECRYPT", "WRAPKEY", "UNWRAPKEY", "DERIVEKEY", "MACGENERATE", "MACVERIFY", "EXPORT", "MANAGE", "AGREEKEY", "AUDIT"}
-                }
-            }
-            app_object["add_groups"] = app_add_group
-        }
-    }
-    if d.HasChange("description") {
-        app_object["description"] = d.Get("description")
-    }
-    //Modifies the existing groups
-    if d.HasChange("mod_group_permissions"){
-        if mod_group := d.Get("mod_group_permissions").(map[string]interface{}); len(mod_group) > 0 {
-            mod_group := d.Get("mod_group_permissions").(map[string]interface{})
-            app_mod_group := make(map[string]interface{})
-            //if default_group has changes in permissions
-            default_group := d.Get("default_group").(string)
-            if perms, ok := mod_group[default_group]; ok {
-                app_mod_group[default_group] = strings.Split(perms.(string), ",")
-            }
-            //checking whether all the group_ids from mod_group_permissions exists in other groups or not
-            //if not it will ignore the mod_group_permissions of the unavailable group_id
-            var other_group_latest []string
-            if err := d.Get("other_group").([]interface{}); len(err) > 0 {
-                for _, group_id := range d.Get("other_group").([]interface{}) {
-                    other_group_latest = append(other_group_latest, group_id.(string))
-                }
-            }
-            for i:=0; i < len(other_group_latest); i++ {
-                if perms, ok := mod_group[other_group_latest[i]]; ok {
-                    app_mod_group[other_group_latest[i]] = strings.Split(perms.(string), ",")
-                }
-            }
-            app_object["mod_groups"] = app_mod_group
-        }
-    }
+	if d.HasChange("default_group") {
+		if default_group := d.Get("default_group").(string); len(default_group) > 0 {
+			app_object["default_group"] = d.Get("default_group")
+		}
+	}
+	if d.HasChange("other_group") {
+		old_group, new_group := d.GetChange("other_group")
+		/*compares the old and new state
+		 * and seggregtes the new groups and groups that are to be deleted.
+		*/
+		add_group_ids, del_group_ids := form_add_and_del_groups(old_group, new_group)
+		//Add the groups to be deleted
+		if len(del_group_ids) > 0 {
+			app_object["del_groups"] = del_group_ids
+		}
+		//Add the new groups
+		if len(add_group_ids) > 0 {
+			add_group_perms := form_group_permissions(d.Get("other_group_permissions"))
+			app_add_group := make(map[string]interface{})
+			for i := 0; i < len(add_group_ids); i++ {
+				if perms, ok := add_group_perms[add_group_ids[i]]; ok {
+					app_add_group[add_group_ids[i]] = perms
+				}else{
+					app_add_group[add_group_ids[i]] = []string{"SIGN", "VERIFY", "ENCRYPT", "DECRYPT", "WRAPKEY", "UNWRAPKEY", "DERIVEKEY", "MACGENERATE", "MACVERIFY", "EXPORT", "MANAGE", "AGREEKEY", "AUDIT"}
+				}
+			}
+			app_object["add_groups"] = app_add_group
+		}
+	}
+	if d.HasChange("description") {
+		app_object["description"] = d.Get("description")
+	}
+	//Modifies the existing groups
+	if d.HasChange("mod_group_permissions"){
+		if mod_group := d.Get("mod_group_permissions").(map[string]interface{}); len(mod_group) > 0 {
+			mod_group := d.Get("mod_group_permissions").(map[string]interface{})
+			app_mod_group := make(map[string]interface{})
+			//if default_group has changes in permissions
+			default_group := d.Get("default_group").(string)
+			if perms, ok := mod_group[default_group]; ok {
+				app_mod_group[default_group] = strings.Split(perms.(string), ",")
+			}
+			//checking whether all the group_ids from mod_group_permissions exists in other groups or not
+			//if not it will ignore the mod_group_permissions of the unavailable group_id
+			var other_group_latest []string
+			if err := d.Get("other_group").([]interface{}); len(err) > 0 {
+				for _, group_id := range d.Get("other_group").([]interface{}) {
+					other_group_latest = append(other_group_latest, group_id.(string))
+				}
+			}
+			for i:=0; i < len(other_group_latest); i++ {
+				if perms, ok := mod_group[other_group_latest[i]]; ok {
+					app_mod_group[other_group_latest[i]] = strings.Split(perms.(string), ",")
+				}
+			}
+			app_object["mod_groups"] = app_mod_group
+		}
+	}
 
-    if len(app_object) > 0 {
-        req, err := m.(*api_client).APICallBody("PATCH", fmt.Sprintf("sys/v1/apps/%s", d.Id()), app_object)
-        if err != nil {
-            diags = append(diags, diag.Diagnostic{
-                Severity: diag.Error,
-                Summary:  "[DSM SDK] Unable to call DSM provider API client",
-                Detail:   fmt.Sprintf("[E]: API: POST sys/v1/apps: %v", err),
-            })
-            return diags
-        }
-        d.SetId(req["app_id"].(string))
-    }
-    return resourceReadApp(ctx, d, m)
+	if len(app_object) > 0 {
+		req, err := m.(*api_client).APICallBody("PATCH", fmt.Sprintf("sys/v1/apps/%s", d.Id()), app_object)
+		if err != nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "[DSM SDK] Unable to call DSM provider API client",
+				Detail:   fmt.Sprintf("[E]: API: POST sys/v1/apps: %v", err),
+			})
+			return diags
+		}
+		d.SetId(req["app_id"].(string))
+	}
+	return resourceReadApp(ctx, d, m)
 }
 
 // [D]: Delete App
@@ -318,59 +315,59 @@ func resourceDeleteApp(ctx context.Context, d *schema.ResourceData, m interface{
 //forms the group permissions - Ravi Gopal
 func form_group_permissions(permissions interface{}) (map[string]interface{}){
 
-    add_group_perms := make(map[string]interface{})
-    if group_perms := permissions.(map[string]interface{}); len(group_perms) > 0 {
-        for group_id, permissions  := range group_perms {
-              permissions_list := strings.Split(permissions.(string), ",")
-              add_group_perms[group_id] = permissions_list
-          }
-    }
+	add_group_perms := make(map[string]interface{})
+	if group_perms := permissions.(map[string]interface{}); len(group_perms) > 0 {
+		for group_id, permissions  := range group_perms {
+			  permissions_list := strings.Split(permissions.(string), ",")
+			  add_group_perms[group_id] = permissions_list
+		  }
+	}
 
-    return add_group_perms
+	return add_group_perms
 }
 
 //forms the add and delete groups during patch request - Ravi Gopal
 func form_add_and_del_groups(old_group interface{}, new_group interface{})([]string, []string){
 
-    /*
-    * Compares old state and new state
-    * seggregtes the groups to be added and groups to be deleted.
-    */
-    old_group_set := old_group.([]interface{})
-    new_group_set := new_group.([]interface{})
+	/*
+	* Compares old state and new state
+	* seggregtes the groups to be added and groups to be deleted.
+	*/
+	old_group_set := old_group.([]interface{})
+	new_group_set := new_group.([]interface{})
 
-    old_group_ids := make([]string, len(old_group_set))
-    for i, v := range old_group_set {
-        old_group_ids[i] = v.(string)
-    }
-    new_group_ids := make([]string, len(new_group_set))
-    for i, v := range new_group_set {
-        new_group_ids[i] = v.(string)
-    }
+	old_group_ids := make([]string, len(old_group_set))
+	for i, v := range old_group_set {
+		old_group_ids[i] = v.(string)
+	}
+	new_group_ids := make([]string, len(new_group_set))
+	for i, v := range new_group_set {
+		new_group_ids[i] = v.(string)
+	}
 
-    new_group_bool := make([]bool, len(new_group_set))
+	new_group_bool := make([]bool, len(new_group_set))
 
-    var del_group_ids []string
-    var add_group_ids []string
+	var del_group_ids []string
+	var add_group_ids []string
 
-    for i := 0; i < len(old_group_ids); i++{
-        exist := false
-        for j := 0; j < len(new_group_ids); j++ {
-            if new_group_ids[j] == old_group_ids[i] {
-                exist = true
-                new_group_bool[j] = true
-                break
-            }
-        }
-        if !exist && len(new_group_ids) > 0 {
-            del_group_ids = append(del_group_ids, old_group_ids[i])
-        }
-    }
-    for i := 0; i < len(new_group_bool); i++ {
-        if !(new_group_bool[i]) {
-            add_group_ids = append(add_group_ids, new_group_ids[i])
-        }
-    }
+	for i := 0; i < len(old_group_ids); i++{
+		exist := false
+		for j := 0; j < len(new_group_ids); j++ {
+			if new_group_ids[j] == old_group_ids[i] {
+				exist = true
+				new_group_bool[j] = true
+				break
+			}
+		}
+		if !exist && len(new_group_ids) > 0 {
+			del_group_ids = append(del_group_ids, old_group_ids[i])
+		}
+	}
+	for i := 0; i < len(new_group_bool); i++ {
+		if !(new_group_bool[i]) {
+			add_group_ids = append(add_group_ids, new_group_ids[i])
+		}
+	}
 
-    return add_group_ids, del_group_ids
+	return add_group_ids, del_group_ids
 }
