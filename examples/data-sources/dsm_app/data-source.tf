f241e307d86789137ce4caee036973e69b18f9d5
Shashidhar Naraparaju <shashidhar.naraparaju@fortanix.com>
2024-07-08 21:02:20 +0530

    5 data sources docs auto generated with tfplugindocs

---
+++ examples/data-sources/dsm_app/data-source.tf
@@ -0,0 +1,4 @@
+data "dsm_app" "sample_app" {
+    app_id = "36cc21d0-a852-4c29-8e34-33388ab73529"
+    new_credential = false
+}
---
+++ examples/data-sources/dsm_aws_group/data-source.tf
@@ -0,0 +1,4 @@
+data "dsm_aws_group" "sample_aws_group" {
+    name = "aws-test"
+    scan = true
+}
