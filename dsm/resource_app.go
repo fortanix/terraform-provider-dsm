550fe3e2a30bc2d747ef43b78db5e0b5d4886b34
RaviGopal <ravi.gopal@fortanix.com>
2024-09-03 21:31:44 +0530

    Revert "Implemented PROD-9031"
    This reverts commit d45e44a9a56b6cca1986c4e064542ff1a0a5737a.

--- dsm/resource_app.go
+++ dsm/resource_app.go
@@ -13,7 +13,6 @@ import (
 	"encoding/base64"
 	"fmt"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -41,7 +40,7 @@ func resourceApp() *schema.Resource {
 			"default_group": {
 			    Description: "The Fortanix DSM group object id to be mapped to the app by default.",
 				Type:     schema.TypeString,
-				Optional: true,
+				Required: true,
 			},
 			"other_group": {
 			    Description: "The Fortanix DSM group object id the app needs to be assigned to. If you want to delete the existing groups from an app, remove the ids during update.",
@@ -101,21 +100,6 @@ func resourceApp() *schema.Resource {
 					Optional: true,
 				},
 			},
-			"role": {
-				Description: "Role of a DSM app. The allowed values are crypto and admin.\n" +
-				"   * `crypto`: To perform the crypto operations.\n" +
-				"   * `admin`: To perform the admin operations.",
-				Type:     schema.TypeString,
-				Optional: true,
-				Default: "crypto",
-				ValidateFunc: validation.StringInSlice([]string{"crypto", "admin"}, true),
-			},
-			"secret_size": {
-				Description: "Size of an API key. Allowed values are 16, 32 and 64. Default value is 64.",
-				Type:     schema.TypeInt,
-				Optional: true,
-				Default: 64,
-			},
 		},
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -127,20 +111,19 @@ func resourceApp() *schema.Resource {
 func resourceCreateApp(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 
-	role := d.Get("role").(string)
 	app_object := map[string]interface{}{
 		"name":          d.Get("name").(string),
+		"default_group": d.Get("default_group").(string),
 		//"add_groups": map[string]interface{}{
 		//	d.Get("default_group").(string): []string{"SIGN", "VERIFY", "ENCRYPT", "DECRYPT", "WRAPKEY", "UNWRAPKEY", "DERIVEKEY", "MACGENERATE", "MACVERIFY", "EXPORT", "MANAGE", "AGREEKEY", "AUDIT"},
 		//},
-		"app_type":    "default",
+		"app_Type":    "default",
 		"description": d.Get("description").(string),
-		"role": role,
-		"secret_size": d.Get("secret_size").(int),
 	}
-	if err := appRoleValidation(d, app_object, role); err != nil {
-	    return err
-	}
+
+	// add groups and it's permissions
+	formAddGroups(d, app_object)
+
 	req, err := m.(*api_client).APICallBody("POST", "sys/v1/apps", app_object)
 	if err != nil {
 		diags = append(diags, diag.Diagnostic{
@@ -175,11 +158,8 @@ func resourceReadApp(ctx context.Context, d *schema.ResourceData, m interface{})
 	if err := d.Set("app_id", req["app_id"].(string)); err != nil {
 		return diag.FromErr(err)
 	}
-	// Admin app will not have default_group
-	if _, ok := req["default_group"]; ok {
-		if err := d.Set("default_group", req["default_group"].(string)); err != nil {
-			return diag.FromErr(err)
-		}
+	if err := d.Set("default_group", req["default_group"].(string)); err != nil {
+		return diag.FromErr(err)
 	}
 	if err := d.Set("acct_id", req["acct_id"].(string)); err != nil {
 		return diag.FromErr(err)
@@ -191,9 +171,6 @@ func resourceReadApp(ctx context.Context, d *schema.ResourceData, m interface{})
 		if err := d.Set("description", req["description"].(string)); err != nil {
 			return diag.FromErr(err)
 		}
-	}
-	if err := d.Set("role", req["role"].(string)); err != nil {
-		return diag.FromErr(err)
 	}
 
 	req, _, err = m.(*api_client).APICall("GET", fmt.Sprintf("sys/v1/apps/%s/credential", d.Id()))
@@ -219,30 +196,21 @@ func resourceReadApp(ctx context.Context, d *schema.ResourceData, m interface{})
 // [U]: Update App
 func resourceUpdateApp(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
-	error_summary := "[DSM SDK] Unable to call DSM provider API client"
+
 	if d.Get("new_credential").(bool) {
 		reset_secret := map[string]interface{}{
 			"credential_migration_period": nil,
-			"secret_size": d.Get("secret_size").(int),
 		}
 
 		_, err := m.(*api_client).APICallBody("POST", fmt.Sprintf("sys/v1/apps/%s/reset_secret", d.Id()), reset_secret)
 		if err != nil {
 			diags = append(diags, diag.Diagnostic{
 				Severity: diag.Error,
-				Summary:  error_summary,
+				Summary:  "[DSM SDK] Unable to call DSM provider API client",
 				Detail:   fmt.Sprintf("[E]: API: GET sys/v1/apps/-/credential: %v", err),
 			})
 			return diags
 		}
-	}
-	if d.HasChange("role") {
-		old_role, new_role := d.GetChange("role")
-		return invokeErrorDiagsWithSummary(error_summary, fmt.Sprintf("[E]: API: PATCH sys/v1/apps/%s: role cannot be changed once it is set. Please retain it to old value: %s -> %s", d.Id(), old_role, new_role))
-	}
-	if !d.Get("new_credential").(bool) && d.HasChange("secret_size") {
-		old_ss, new_ss := d.GetChange("secret_size")
-		return invokeErrorDiagsWithSummary(error_summary, fmt.Sprintf("[E]: API: PATCH sys/v1/apps/%s: secret_size cannot be changed once it is set. It can be updated only when new_credential is initiated. Please retain it to old value: %s -> %s", d.Id(), old_ss, new_ss))
 	}
 	//Modified by Ravi Gopal
 	app_object := make(map[string]interface{})
@@ -271,7 +239,7 @@ func resourceUpdateApp(ctx context.Context, d *schema.ResourceData, m interface{
 		if err != nil {
 			diags = append(diags, diag.Diagnostic{
 				Severity: diag.Error,
-				Summary:  error_summary,
+				Summary:  "[DSM SDK] Unable to call DSM provider API client",
 				Detail:   fmt.Sprintf("[E]: API: POST sys/v1/apps: %v", err),
 			})
 
